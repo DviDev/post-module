f86754d5508a71cf2aaca8ea6a6acbe563f29624
Davi Menezes <davimenezes.dev@gmail.com>
2022-11-05 15:01:28 -0300

    refactorings upgrade base structure

---
+++ Database/Factories/PostTagFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modules\Post\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\Post\Entities\PostTag\PostTagEntityModel;
+use Modules\Post\Models\PostTagModel;
+
+/**
+ * @method PostTagModel create(array $attributes = [])
+ * @method PostTagModel make(array $attributes = [])
+ */
+class PostTagFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = PostTagModel::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        $p = PostTagEntityModel::props(null, true);
+        return [
+
+        ];
+    }
+}
---
+++ Entities/PostComment/PostCommentProps.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Post\Entities\PostComment;
+
+/**
+ * @property $id
+ * @property $post_id
+ * @property $parent_id
+ * @property $content
+ * @property $user_id
+ * @property $created_at
+ */
+trait PostCommentProps
+{
+
+}
