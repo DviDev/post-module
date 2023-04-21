b391b6037fc0e4573060c2a19c3420d1895484e9
Davi Menezes <davimenezes.dev@gmail.com>
2023-04-21 19:30:44 -0300

    post routes moved to post-module

--- Routes/web.php
+++ Routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::prefix('post')->group(function() {
-    Route::get('/', 'PostController@index');
+use Modules\Post\Models\PostCommentModel;
+use Modules\Post\Models\PostModel;
+
+Route::prefix('post')->group(function () {
+    Route::view('/list', 'post::components.page.postlistpage')->name('admin.posts');
+    Route::get('/{post}/comments',
+        fn(PostModel $post) => view('post::components.page.postcommentpage', compact('post')))
+            ->name('admin.post.comments');
+    Route::get('/comment/{comment}/votes',
+        fn(PostCommentModel $comment) => view('post::components.page.comment_votes_page', compact('comment')))
+            ->name('admin.post.comment.votes');
+    Route::get('/{post}/tags', fn(PostModel $post) => view('post::components.page.tags_page', compact('post')))
+        ->name('admin.post.tags');
+    Route::get('/{post}/votes', fn(PostModel $post) => view('post::components.page.votes_page', compact('post')))
+        ->name('admin.post.votes');
+
+    Route::get('/form/{post}', fn(PostModel $post) => view('post::components.page.postform', compact($post)))
+        ->name('admin.post.form');
 });
