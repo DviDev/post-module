399b371fd8f0a272aa2eb694be289a1c898e3846
Davi Menezes <davimenezes.dev@gmail.com>
2023-07-27 23:46:36 -0300

    🪴seeder improvements

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-    'name' => 'Post'
+    'name' => 'Post',
+    'SEED_POST_COMMENT_VOTES_COUNT' => 3,
+    'SEED_POST_COMMENTS_COUNT' => 3,
+    'SEED_POST_TAGS_COUNT' => 3,
+    'SEED_POST_VOTES_COUNT' => 3,
+    'SEED_POSTS_COUNT' => 3,
 ];
