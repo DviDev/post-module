eb12197c37f61a3ac02a5b31f2f78707c297eea7
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-13 17:08:48 -0300

    Refactor `HasMessage` to use `createWithType` for `RecordModel` creation logic. Add `ElementModel` for better type handling.

--- app/Services/Message/HasMessage.php
+++ app/Services/Message/HasMessage.php
@@ -5,17 +5,18 @@ namespace Modules\Post\Services\Message;
 use Modules\Base\Models\RecordModel;
 use Modules\Base\Models\RecordRelationModel;
 use Modules\Post\Models\ThreadModel;
+use Modules\View\Models\ElementModel;
 
 trait HasMessage
 {
     public function addMessage(ThreadModel $comment): ThreadModel
     {
         if (! $this->record_id) {
-            $this->record_id = RecordModel::create()->id;
+            $this->record_id = RecordModel::createWithType(type: ElementModel::class)->id;
             $this->save();
         }
 
-        $commentItem = RecordModel::create();
+        $commentItem = RecordModel::createWithType(type: ElementModel::class);
         RecordRelationModel::create(['record1' => $this->record_id, 'record2' => $commentItem->id]);
 
         $comment->record_id = $commentItem->id;
