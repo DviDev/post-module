b229cf2c199b17768097f01c196abb2128f95438
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-05 13:00:13 -0300

    Add Portuguese language translations for `thread_posts` in `Post` module

--- lang/pt_BR/thread_posts.php
+++ lang/pt_BR/thread_posts.php
@@ -1,1 +1,14 @@
 <?php
+
+return [
+    'id' => 'id',
+    'record_id' => 'registro',
+    'user_id' => 'usuario',
+    'thread_id' => 'thread',
+    'title' => 'título',
+    'content' => 'conteúdo',
+    'thumbnail_image_path' => 'caminho da imagem',
+    'created_at' => 'data de criação',
+    'updated_at' => 'data de atualização',
+    'deleted_at' => 'data de exclusão',
+];
