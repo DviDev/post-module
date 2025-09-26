a4c9f7cc8c47a1db34b148b45ba2c5dbcb1fce59
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-26 15:50:35 -0300

    Refactor to replace BaseEntityModel with BaseEntityModel Contract
    Replaced the usage of `Modules\Base\Entities\BaseEntityModel` with `Modules\Base\Contracts\BaseEntityModel` across multiple entity models to align with the contract-driven design approach. This ensures greater flexibility and adherence to interface-driven development practices.

--- app/Entities/Post/PostEntityModel.php
+++ app/Entities/Post/PostEntityModel.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Post\Entities\Post;
 
-use Modules\Base\Entities\BaseEntityModel;
+use Modules\Base\Contracts\BaseEntityModel;
 use Modules\Post\Models\PostModel;
 use Modules\Post\Repositories\PostRepository;
 
