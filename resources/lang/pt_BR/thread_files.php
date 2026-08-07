02f616e4a2b87b47312026e3a82e58bbd4771c5d
Davi Menezes <davimenezes.dev@gmail.com>
2026-08-07 19:43:51 -0300

    lang path improvements to phpstorm autocomplete

---
+++ resources/lang/pt_BR/thread_files.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'id' => 'id',
+    'thread_id' => 'fio',
+    'file_path' => 'caminho do arquivo',
+    'created_at' => 'data de criação',
+    'updated_at' => 'data de atualização',
+    'deleted_at' => 'data de exclusão',
+];
---
+++ resources/lang/pt_BR/thread_votes.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'id' => 'id',
+    'thread_id' => 'fio',
+    'user_id' => 'usuário',
+    'like' => 'gostei',
+    'dislike' => 'não gostei',
+    'created_at' => 'data de criação',
+];
